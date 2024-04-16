d7634ccf861bb93b85dc6865be8ddea78254a762
Victor <freelance2@eviciti.com.mx>
2024-04-16 10:42:26 -0600

    Cambios en vistas de Recuperación de contraseña y Coach

---
+++ Controllers/CoachController.cs
@@ -0,0 +1,39 @@
+using CrafterCodes.Data;
+using CrafterCodes.Models;
+using CrafterCodes.Models.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+using System.Security.Claims;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CrafterCodes.Controllers
+{
+    public class CoachController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        
+        private readonly Contexto _contexto;
+        private readonly ApplicationDbContext _context;
+
+        public CoachController(ILogger<HomeController> logger, Contexto contexto)
+        {
+            _logger = logger;
+            _contexto = contexto;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+    }
+}
